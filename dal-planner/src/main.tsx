556e585d2b60ebe011a492d50224c7dd47d65a18
Quinn <quinnedwards22@gmail.com>
2026-03-19 10:08:45 -0300

    remove posthog cookies

--- dal-planner/src/main.tsx
+++ dal-planner/src/main.tsx
@@ -7,6 +7,7 @@ import App from './App.tsx'
 const posthogOptions = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2026-01-30',
+  cookieless_mode: 'always',
 } as const
 
 createRoot(document.getElementById('root')!).render(
